cfcaf988f0280531450fede7c297b4077dc48b7d
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2021-09-08 23:16:10 -0400

    first commit

---
+++ src/UserSearch.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const users = [
+  { name: "brooks", age: 25 },
+  { name: "sandy", age: 20 },
+  { name: "alex", age: 20 },
+];
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number | undefined }>();
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Search </button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+export default UserSearch;
